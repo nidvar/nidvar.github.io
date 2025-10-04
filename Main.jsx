c0a79f231968385f744d43ed665f76e301a281ef
Nidvar <djayshine87@hotmail.com>
2025-10-05 09:45:38 +1100

    Delete Main.jsx

--- Main.jsx
+++
@@ -1,9 +0,0 @@
-class Main extends React.Component{
-    render(){
-        return(
-            <div className='Main'>
-                <h1><a href='https://nidvar.netlify.app/'>CLICK ME</a></h1>
-            </div>
-        )
-    }
-}
