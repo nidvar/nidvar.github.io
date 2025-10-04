20e6ea066aed64fe956b8ac30dc00d37a8e22aab
Nidvar <djayshine87@hotmail.com>
2025-10-05 09:42:49 +1100

    Delete Boxes.jsx

--- Boxes.jsx
+++
@@ -1,9 +0,0 @@
-class Boxes extends React.Component{
-    render(){
-        return(
-            <div className={this.props.className}>
-                <a href={this.props.source}><img src={this.props.image} /></a>
-            </div>
-        )
-    }
-}
