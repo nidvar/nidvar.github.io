be6234551b9edc5f0a69bad92664fade3a93b3f4
nidvar <djayshine87@hotmail.com>
2025-11-25 19:16:18 +1100

    style changes. using javascript to load data

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('portfolio website')
+const projects = [
+    {
+        title: "Reddit clone",
+        link: "https://jmern.vercel.app/",
+        description: "Login, password authentication and database",
+        tech: "React - TypeScript - Node - MongoDB"
+    },
+    {
+        title: "Whatsapp clone",
+        link: "https://jarrochat.vercel.app/",
+        description: "Login, password authentication and database",
+        tech: "React - TypeScript - Node - MongoDB"
+    },
+];
+
+const container = document.querySelector(".container");
+
+projects.forEach(project => {
+    const section = document.createElement("section");
+
+    section.innerHTML = `
+    <a href="${project.link}" target="_blank">
+        <p class="heading light-blue">
+        ${project.title}
+        </p>
+        <p>${project.description}</p>
+        <p><span class="bold smaller">${project.tech}</span></p>
+    </a>
+  `;
+
+    container.appendChild(section);
+});
