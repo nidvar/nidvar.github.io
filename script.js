5bffc4647caa1554e7a57511b4620db0c3f9d3f9
nidvar <djayshine87@hotmail.com>
2026-02-04 17:42:27 +1100

    spacing change, added render warning

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const projects = [
-    {
-        title: "Netflix clone",
-        link: "https://jarro-netflix.onrender.com/",
-        description: "Full stack. Simple Netflix clone",
-        tech: "React - TypeScript - Node - PostgreSQL"
-    },
-    {
-        title: "Reddit clone",
-        link: "https://jmern.vercel.app/",
-        description: "Full stack. Create, update, comment on posts",
-        tech: "React - TypeScript - Node - MongoDB"
-    },
-    {
-        title: "Whatsapp clone",
-        link: "https://jarro-chat.onrender.com/",
-        description: "Full stack. Real time chat with websockets",
-        tech: "React - TypeScript - Node - MongoDB"
-    },
+  {
+    title: "Netflix clone",
+    link: "https://jarro-netflix.onrender.com/",
+    description: "Full stack. Simple Netflix clone",
+    tech: "React - TypeScript - Node - PostgreSQL",
+  },
+  {
+    title: "Reddit clone",
+    link: "https://jmern.vercel.app/",
+    description: "Full stack. Create, update, comment on posts",
+    tech: "React - TypeScript - Node - MongoDB",
+  },
+  {
+    title: "Whatsapp clone",
+    link: "https://jarro-chat.onrender.com/",
+    description: "Full stack. Real time chat with websockets",
+    tech: "React - TypeScript - Node - MongoDB",
+  },
 ];
 
 const container = document.querySelector(".container");
 
-projects.forEach(project => {
-    const section = document.createElement("section");
+projects.forEach((project) => {
+  const section = document.createElement("section");
 
-    section.innerHTML = `
+  section.innerHTML = `
     <a href="${project.link}" target="_blank">
         <p class="heading light-blue">
         ${project.title}
@@ -34,5 +34,5 @@ projects.forEach(project => {
     </a>
   `;
 
-    container.appendChild(section);
+  container.appendChild(section);
 });
